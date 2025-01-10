e2b41b1e84a33440cc6bc1f7279f3d1b11d586cc
c_ <c_@localhost.localdomain>
2025-01-10 12:01:17 +0800

    added script to full from repo

---
+++ pull.js
@@ -0,0 +1,18 @@
+/** @param { NS } ns */
+
+export async function main(ns) {
+	await ns.wget(
+		"https://api.github.com/repos/CUnderscore/bitburner.c/git/trees/main",
+		"gitMain.js",
+		"home",
+	);
+	const filter = new RegExp(/\w+.js/g);
+	const toPull = ns.read("gitMain.js").match(filter);
+	for (const file of toPull) {
+		await ns.wget(
+			`https://raw.githubusercontent.com/CUnderscore/bitburner.c/refs/heads/main/${file}`,
+			file,
+			"home",
+		);
+	}
+}
