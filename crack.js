0c932dfa84f38273450615728945dc5ef6498231
c_ <c_@localhost.localdomain>
2025-01-10 14:51:58 +0800

    port opening script

--- crack.js
+++ crack.js
@@ -1,24 +1,31 @@
 /** @param { NS } ns */
 
 export async function main(ns) {
-	function openPorts(server) {
-		const ports = new Map([
-			["BruteSSH", ns.brutessh],
-			["FTPCrack", ns.ftpcrack],
-			["relaySMTP", ns.relaysmtp],
-			["HTTPWorm", ns.httpworm],
-			["SQLInject", ns.sqlinject],
-		]);
-		for (const [file, cmd] of ports) {
-			if (ns.fileExists(`${file}.exe`)) {
-				cmd(server);
-			}
-		}
-		return ns.getServer(server).numPortsOpen;
-	}
-	function nuke(server) {
-		const _server = server;
-		if (_server.numPortsOpen >= _server.numPortsRequired) {
-		}
-	}
+    const victim = ns.args[0]
+    openPorts(victim);
+    nuke(victim);
+
+
+    function openPorts(server) {
+        const ports = new Map([
+            ["BruteSSH", ns.brutessh],
+            ["FTPCrack", ns.ftpcrack],
+            ["relaySMTP", ns.relaysmtp],
+            ["HTTPWorm", ns.httpworm],
+            ["SQLInject", ns.sqlinject],
+        ]);
+        for (const [file, cmd] of ports) {
+            if (ns.fileExists(`${file}.exe`)) {
+                cmd(server);
+            }
+        }
+        return ns.getServer(server).numPortsOpen;
+    }
+    function nuke(server) {
+        const _server = ns.getServer(server);
+        if (_server.numPortsOpen >= _server.numPortsRequired) {
+            ns.nuke(_server.name);
+            return true;
+        } return false;
+    }
 }
