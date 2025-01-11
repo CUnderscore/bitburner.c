c73d0c5fd22df24f18ba0d75b273880bdb475dde
c_ <c_@localhost.localdomain>
2025-01-11 19:36:47 +0800

    caesar cipher solver

--- cc_get.js
+++ cc_get.js
@@ -2,5 +2,30 @@ import { getServers } from "./scan";
 /** @param {NS} ns */
 
 export async function main(ns) {
-	const servers = getServers(ns, "serverlist.txt");
+    const servers = getServers(ns, "serverlist.txt");
+}
+
+function caesarCipher(problem) {
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let solution = "";
+    for (const ch of problem[0]) {
+        if (isWhitespace(ch)) {
+            solution += " ";
+        } else {
+            solution += shiftLetter(ch, problem[1]);
+        }
+    }
+    return solution;
+
+    function isWhitespace(letter) {
+        return /\s/.test(letter);
+    }
+
+    function shiftLetter(letter, shift) {
+        const position = alphabet.indexOf(letter) - shift;
+        if (position < 0) {
+            return alphabet.charAt(alphabet.length + position);
+        }
+        return alphabet.charAt(position);
+    }
 }
